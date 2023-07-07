69d05c918853e39a4c677038435b2bb7844b4c88
Hashim66 <f190349@nu.edu.pk>
2023-07-07 19:59:03 +0500

    Header
    This is the festifyer header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Task from './components/Task';
+
+function MyApp() {
+  return (
+    <div className="App">
+    <Task/>
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ src/components/Task.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../design/Task.css"
+
+
+const MyApp = () => {
+  return (
+    <>
+      <task>
+      <img src="https://www.festifyer.com/storage/system/6fYpjT6r6ugsHt5aYXOm59JeRHk7OslnDU3yktbl.png" alt="alternative"/>
+        <div>
+          <ul id='task1' >
+            <li><a href="/">Blogs</a></li>
+            <li><a href="/">Contact</a></li>
+            <button>Login</button>
+            
+          </ul>
+        </div>
+
+        <div id="x" >
+        </div>
+      </task>
+    </>
+  )
+}
+
+export default MyApp;
